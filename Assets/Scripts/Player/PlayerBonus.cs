0da35cfad50db89ec164b9f3fa07a3130a56f9f5
Chloe-Ms <mllechloe.mathias@gmail.com>
2022-11-08 13:26:43 +0100

    Yellow bonus is working

--- Assets/Scripts/Player/PlayerBonus.cs
+++ Assets/Scripts/Player/PlayerBonus.cs
@@ -16,8 +16,16 @@ public class PlayerBonus : MonoBehaviour
 {
     [SerializeField] PlayerAttack _attackScript;
     [SerializeField] PlayerMovement _movementScript;
+    [SerializeField] private GameObject _shield;
     List<BonusType> _bonuses;
-    
+    //YELLOW
+    private float _durationY;
+    private float _currentDurationY;
+    private bool _isActiveY = false;
+    //GREEN
+    private float _durationG;
+    private float _currentDurationG;
+    private bool _isActiveG = false;
 
     void Start()
     {
@@ -56,6 +64,49 @@ public class PlayerBonus : MonoBehaviour
         }
     }
 
+    public void ApplyBlueBonus()
+    {
+        if (_bonuses.Contains(BonusType.BLUE))
+        {
+            _bonuses.Remove(BonusType.BLUE);
+            _attackScript.AddBlueBonus();
+            _attackScript.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+        else
+        {
+            Debug.Log("bleu déjà fait");
+        }
+    }
+
+    public void ApplyYellowBonus(float duration)
+    {
+        if (_bonuses.Contains(BonusType.YELLOW))
+        {
+            
+            _bonuses.Remove(BonusType.YELLOW);
+            _durationY = duration;
+            StartTimerBonusY();
+            _attackScript.GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+        else
+        {
+            Debug.Log("jaune déjà fait");
+        }
+    }
+
+    public void ApplyGreenBonus()
+    {
+        if (_bonuses.Contains(BonusType.GREEN))
+        {
+            _bonuses.Remove(BonusType.GREEN);
+
+            _attackScript.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+        else
+        {
+            Debug.Log("vert déjà fait");
+        }
+    }
     public void ClearBonuses()
     {
         foreach (BonusType bonus in Enum.GetValues(typeof(BonusType)))
@@ -66,21 +117,38 @@ public class PlayerBonus : MonoBehaviour
         _attackScript.ResetOutputProjectile();
         //BLACK
         _movementScript.ResetMovementVertically();
+        //YELLOW
+        EndTimerBonusY();
+        //GREEN
+        EndTimerBonusG();
     }
 
-    public void ApplyBlueBonus()
+    public void StartTimerBonusY()
     {
-        if (_bonuses.Contains(BonusType.BLUE))
-        {
-            _bonuses.Remove(BonusType.BLUE);
-            _attackScript.AddBlueBonus();
-            _attackScript.GetComponent<SpriteRenderer>().color = Color.blue;
-        } else
-        {
-            Debug.Log("bleu déjà fait");
-        }
+        _currentDurationY = 0f;
+        _isActiveY = true;
+        _shield.SetActive(true);
     }
 
+    public void StartTimerBonusG()
+    {
+        _currentDurationG = 0f;
+        _isActiveG = true;
+    }
+    public void EndTimerBonusY()
+    {
+        _isActiveY = false;
+        _shield.SetActive(false);
+        _bonuses.Add(BonusType.YELLOW);
+    }
+
+    public void EndTimerBonusG()
+    {
+        _isActiveG = false;
+        _bonuses.Add(BonusType.GREEN);
+    }
+
+    #region LIST BONUS
     public int GetBonusesSize()
     {
         return _bonuses.Count;
@@ -108,5 +176,37 @@ public class PlayerBonus : MonoBehaviour
     {
         Array values = Enum.GetValues(typeof(BonusType));
         return (BonusType) values.GetValue(index);
+    }
+    public int GetSizeBonusEnum()
+    {
+        Array values = Enum.GetValues(typeof(BonusType));
+        return values.Length;
+    }
+    #endregion
+
+    private void Update()
+    {
+        if (_isActiveY)
+        {
+            if (_currentDurationY < _durationY)
+            {
+                _currentDurationY += Time.deltaTime;
+            }
+            else
+            {
+                EndTimerBonusY();
+            }
+        }
+        if (_isActiveG)
+        {
+            if (_currentDurationG < _durationG)
+            {
+                _currentDurationG += Time.deltaTime;
+            }
+            else
+            {
+                EndTimerBonusG();
+            }
+        }
     }
 }
